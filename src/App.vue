039d605dfa7f081ba50f25b389e10d67e169d4df
pavel <shirokuiuna@mail.ru>
2023-07-28 14:06:43 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="container">
+    <div class="right-container">
+      <div class="gantt-selected-info">
+        <div v-if="selectedTask">
+          <h2>{{ selectedTask.text }}</h2>
+          <span><b>ID: </b>{{ selectedTask.id }}</span
+          ><br />
+          <span><b>Progress: </b> {{ progressPercentage }}</span
+          ><br />
+          <span><b>Start Date: </b
+            >{{ formattedStartDate }}</span
+          ><br />
+          <span><b>End Date: </b>{{ formattedEndDate }}</span
+          ><br />
+        </div>
+        <div v-else class="select-task-prompt">
+          <h2>Click any task</h2>
+        </div>
+      </div>
+      <ul class="gantt-messages">
+        <li
+          class="gantt-message"
+          v-for="(message, index) in messages"
+          v-bind:key="index"
+        >
+          {{ message }}
+        </li>
+      </ul>
+    </div>
+    <GanttComponent
+      class="left-container"
+      :tasks="tasks"
+      @task-updated="logTaskUpdate"
+      @link-updated="logLinkUpdate"
+      @task-selected="selectTask"
+    ></GanttComponent>
+  </div>
+</template>
+
+<script>
+import GanttComponent from "./components/GanttComponent.vue";
+
+export default {
+  name: "app",
+  components: { GanttComponent },
+  data() {
+    return {
+      tasks: {
+        data: [
+          {
+            id: 1,
+            text: "Task #1",
+            start_date: "2020-01-17",
+            duration: 3,
+            progress: 0.6,
+          },
+          {
+            id: 2,
+            text: "Task #2",
+            start_date: "2020-01-20",
+            duration: 3,
+            progress: 0.4,
+          },
+        ],
+        links: [{ id: 1, source: 1, target: 2, type: "0" }],
+      },
+      selectedTask: null,
+      messages: [],
+    };
+  },
+  computed: {
+    progressPercentage() {
+      let taskProgress = this.selectedTask.progress;
+      if (!taskProgress) {
+        return "0";
+      }
+      return `${Math.round(+taskProgress * 100)} %`;
+    },
+    formattedStartDate() {
+      let taskStart = this.selectedTask.start_date;
+      return `${taskStart.getFullYear()} / ${taskStart.getMonth() + 1} / ${taskStart.getDate()}`;
+    },
+    formattedEndDate() {
+      let taskEnd = this.selectedTask.end_date;
+      return `${taskEnd.getFullYear()} / ${taskEnd.getMonth() + 1} / ${taskEnd.getDate()}`;
+    },
+  },
+  methods: {
+    selectTask(task) {
+      this.selectedTask = task;
+    },
+
+    addMessage(message) {
+      this.messages.unshift(message);
+      if (this.messages.length > 40) {
+        this.messages.pop();
+      }
+    },
+
+    logTaskUpdate(id, mode, task) {
+      let text = task && task.text ? ` (${task.text})` : "";
+      let message = `Task ${mode}: ${id} ${text}`;
+      this.addMessage(message);
+    },
+
+    logLinkUpdate(id, mode, link) {
+      let message = `Link ${mode}: ${id}`;
+      if (link) {
+        message += ` ( source: ${link.source}, target: ${link.target} )`;
+      }
+      this.addMessage(message);
+    },
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+}
+
+.left-container {
+  overflow: hidden;
+  position: relative;
+  height: 100%;
+}
+
+.right-container {
+  border-right: 1px solid #cecece;
+  float: right;
+  height: 100%;
+  width: 340px;
+  box-shadow: 0 0 5px 2px #aaa;
+  position: relative;
+  z-index: 2;
+}
+
+.gantt-messages {
+  list-style-type: none;
+  height: 50%;
+  margin: 0;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-left: 5px;
+}
+
+.gantt-messages > .gantt-message {
+  background-color: #f4f4f4;
+  box-shadow: inset 5px 0 #d69000;
+  font-family: Geneva, Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  margin: 5px 0;
+  padding: 8px 0 8px 10px;
+}
+
+.gantt-selected-info {
+  border-bottom: 1px solid #cecece;
+  box-sizing: border-box;
+  font-family: Geneva, Arial, Helvetica, sans-serif;
+  height: 50%;
+  line-height: 28px;
+  padding: 10px;
+}
+
+.gantt-selected-info h2 {
+  border-bottom: 1px solid #cecece;
+}
+
+.select-task-prompt h2 {
+  color: #d9d9d9;
+}
+</style>
---
+++ src/components/GanttComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="ganttContainer"></div>
+</template>
+
+<script>
+import { gantt } from 'dhtmlx-gantt';
+
+export default {
+  props: {
+    tasks: {
+      type: Object,
+      default () {
+        return {data: [], links: []}
+      }
+    }
+  },
+
+  methods: {
+    $_initGanttEvents() {
+      if (!gantt.$_eventsInitialized) {
+        gantt.attachEvent('onTaskSelected', (id) => {
+          let task = gantt.getTask(id);
+          this.$emit('task-selected', task);
+        });
+
+        gantt.attachEvent('onTaskIdChange', (id, new_id) => {
+          if (gantt.getSelectedId() == new_id) {
+            let task = gantt.getTask(new_id);
+            this.$emit('task-selected', task);
+          }
+        });
+
+        gantt.$_eventsInitialized = true;
+      }
+    },
+
+    $_initDataProcessor() {
+      if (!gantt.$_dataProcessorInitialized) {
+        gantt.createDataProcessor((entity, action, data, id) => { 
+          this.$emit(`${entity}-updated`, id, action, data);
+        });
+
+        gantt.$_dataProcessorInitialized = true;
+      }
+    }
+  },
+
+  mounted() {
+    this.$_initGanttEvents();
+    gantt.config.date_format = "%Y-%m-%d";
+
+    gantt.init(this.$refs.ganttContainer);
+    gantt.parse(this.$props.tasks);
+
+    this.$_initDataProcessor();
+  }
+}
+</script>
+
+<style>
+    @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+</style>
